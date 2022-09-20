2bc2687169cedb34abedc7833cf018e958a124c1
ABDULRAHMAN IMAM <111951966+Abdulrahman-imam@users.noreply.github.com>
2022-09-20 06:12:45 +0100

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,18 +1,10 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-/**
- * File: main.h
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x04-pointers_arrays_strings directory.
- */
-
-#include <stdlib.h>
-
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -22,4 +14,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#end if
+#endif
